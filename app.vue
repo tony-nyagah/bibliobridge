90cf7e4be7d95c78b92dd090ed9e493460539756
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-11-26 21:29:25 +0300

    initial setup - includes tailwind config

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  const version: number = 3;
+</script>
+
+<template>
+  <div class="hello">
+    <p class="text-red-500">Hello Nuxt {{ version }}</p>
+  </div>
+</template>
+
+<style scoped>
+  .hello {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 3rem;
+    padding: 2rem;
+  }
+</style>
