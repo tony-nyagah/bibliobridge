545d2a34b76e416764617510ca97034c4e4cd160
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-11-28 11:55:31 +0300

    add daisy ui to dependencies

--- app.vue
+++ app.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-  const version: number = 3;
-</script>
-
 <template>
-  <div class="hello">
-    <p class="text-red-500">Hello Nuxt {{ version }}</p>
+  <div class="p-4">
+    <button class="btn">Hello daisyUI</button>
   </div>
 </template>
-
-<style scoped>
-  .hello {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 3rem;
-    padding: 2rem;
-  }
-</style>
